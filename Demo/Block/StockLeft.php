e83237e0e44789454be0e86cb2ad5bf4668a847f
Yash Tiwari <yash.tiwari@pheuture.com>
2022-03-23 13:59:08 +0530

    Starter template for Magento Module

---
+++ Demo/Block/StockLeft.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Pheuture\Demo\Block;
+
+use Magento\Framework\View\Element\Template;
+
+class StockLeft extends Template {
+    
+    public function getDate() {
+
+        return "Yash";
+    }
+}
